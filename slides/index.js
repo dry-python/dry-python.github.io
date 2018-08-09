5fb35822a9f4a911fe0600b92afccb75106a7bd4
Artem Malyshev <proofit404@gmail.com>
2018-08-09 05:55:12 +0300

    Tweak Reveal.js initialize options.

--- slides/index.js
+++ slides/index.js
@@ -2,4 +2,11 @@ import 'reveal.js/css/reveal.css'
 import 'reveal.js/css/theme/league.css'
 import Reveal from 'reveal.js/js/reveal.js'
 
-Reveal.initialize()
+Reveal.initialize({
+    controls: false,
+    progress: false,
+    slideNumber: true,
+    history: true,
+    center: true,
+    transition: 'none'
+})
