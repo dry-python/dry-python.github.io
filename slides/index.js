f539aaa24d34917f359eb080d3b37d622a435ddd
Artem Malyshev <proofit404@gmail.com>
2018-09-13 12:29:12 +0300

    Highlight source code in the slides.

--- slides/index.js
+++ slides/index.js
@@ -1,6 +1,11 @@
+import 'highlight.js/styles/zenburn.css'
+import hljs from 'highlight.js'
+
 import 'reveal.js/css/reveal.css'
 import 'reveal.js/css/theme/beige.css'
 import Reveal from 'reveal.js/js/reveal.js'
+
+hljs.initHighlightingOnLoad()
 
 Reveal.initialize({
     controls: false,
