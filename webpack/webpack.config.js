f10987109e24155203a68549a88324bcb48c9877
Artem Malyshev <proofit404@gmail.com>
2018-12-02 06:31:55 +0300

    Split site and slides bundles.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,19 +1,14 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 
 module.exports = {
   entry: {
-    slides: '../slides/index.js',
     styles: './styles/main.css'
   },
   output: {
-    path: path.resolve(__dirname, '../assets/static/slides/'),
+    path: path.resolve(__dirname, '../assets/static/'),
     filename: '[name].js'
-  },
-  resolve: {
-    modules: [path.resolve(__dirname, 'node_modules')]
   },
   mode: 'production',
   devtool: 'source-map',
@@ -24,25 +19,12 @@ module.exports = {
         use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.pug$/,
-        use: 'pug-loader'
-      },
-      {
         test: /\.(woff|woff2|eot|ttf|otf|png|jpg)$/,
         use: 'file-loader'
       }
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: '../slides/index.pug',
-      chunks: ['slides']
-    }),
     new MiniCssExtractPlugin({
       filename: '[name].css'
     })
