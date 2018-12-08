c8f650557f4dd5c136f08fa91362573c89b2bf03
Artem Malyshev <proofit404@gmail.com>
2018-12-08 18:05:28 +0300

    Add gitter sidecar to the bundle.

--- webpack/app/index.js
+++ webpack/app/index.js
@@ -1,5 +1,9 @@
 import './index.css'
 import Turbolinks from 'turbolinks'
+import Sidecar from 'gitter-sidecar'
 
 
 Turbolinks.start()
+
+
+let chatButton = new Sidecar({room: 'dry-python/dry-python'})
