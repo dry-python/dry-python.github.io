cb09600d7d3707052252894458601855c329db38
Artem Malyshev <proofit404@gmail.com>
2018-12-08 17:12:13 +0300

    Use right babel preset.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -21,7 +21,12 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf|png|jpg)$/,
