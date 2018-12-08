3b4d5c09639733b12e26035646c5dda7599ff23e
Artem Malyshev <proofit404@gmail.com>
2018-12-08 18:45:16 +0300

    Use highlight.js for source code on the page.

--- webpack/app/index.js
+++ webpack/app/index.js
@@ -1,9 +1,21 @@
-import './index.css'
 import Turbolinks from 'turbolinks'
 import Sidecar from 'gitter-sidecar'
+import hljs from 'highlight.js/lib/highlight'
+import python from 'highlight.js/lib/languages/python'
+import 'highlight.js/styles/github.css'
+
+import './index.css'
 
 
 Turbolinks.start()
 
-
 let chatButton = new Sidecar({room: 'dry-python/dry-python'})
+
+hljs.registerLanguage('python', python);
+
+document.addEventListener('turbolinks:load', function() {
+  let blocks = document.getElementsByTagName('code')
+  for (let block of blocks) {
+    hljs.highlightBlock(block)
+  }
+})
