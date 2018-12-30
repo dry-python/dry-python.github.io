cd19147a6d570008fbf54bc1e4ef7147cee979b7
Artem Malyshev <proofit404@gmail.com>
2018-12-30 05:39:15 +0300

    Fix gitter glitch on the initial page load.

--- webpack/app/index.js
+++ webpack/app/index.js
@@ -16,6 +16,15 @@ document.addEventListener('turbolinks:load', () => {
     targetElement: '.gitter-chat-embed',
     activationElement: '.gitter-open-chat-button'
   })
+
+  // Initial page load isn't fast enough.  Gitter create it's styles
+  // at runtime.  So first page view will have slight glitch when
+  // Gitter chat popup quickly open and close.  We put "display:
+  // none;" style directly on the element node to prevent it.  After
+  // we initiate the chat we need to remove it.
+  for (let elem of document.getElementsByClassName('gitter-chat-embed')) {
+    elem.removeAttribute('style')
+  }
 })
 
 document.addEventListener('turbolinks:load', () => {
