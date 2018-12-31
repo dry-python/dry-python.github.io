f2d9c14796c6990529c0c9aaed03d46c8bfbf5bd
Artem Malyshev <proofit404@gmail.com>
2018-12-31 04:17:41 +0300

    Rewrite tailwind config without top level require.

--- webpack/postcss.config.js
+++ webpack/postcss.config.js
@@ -1,9 +1,6 @@
-const tailwindcss = require('tailwindcss')
-
-
 module.exports = {
   plugins: [
-    tailwindcss('./tailwind.config.js'),
+    require('tailwindcss')('./tailwind.config.js'),
     require('autoprefixer')
   ]
 }
