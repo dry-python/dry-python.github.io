7f7eb775a823888a093073f58356f615e708a2ad
Artem Malyshev <proofit404@gmail.com>
2019-05-23 14:09:15 +0300

    Fix stylelint errors.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   extends: 'stylelint-config-standard',
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+        ],
+      },
+    ],
+  },
 };
