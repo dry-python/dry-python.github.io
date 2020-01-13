050a306fe04154f02e631cb2611c33d7fe2d4b0c
Artem Malyshev <proofit404@gmail.com>
2020-01-13 23:07:26 +0300

    Remove Lektor project.

--- app/dry-python/controllers/navbar_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from 'stimulus';
-
-export default class extends Controller {
-  static targets = ['menuItems'];
-
-  toggle() {
-    this.menuItemsTarget.classList.toggle('hidden');
-  }
-}
