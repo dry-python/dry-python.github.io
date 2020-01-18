1b71c8249924829a6cd34e01b3939bc2edfb820f
Artem Malyshev <proofit404@gmail.com>
2020-01-19 02:02:56 +0300

    Setup black and flake8 pre-commit hooks.

--- tasks.py
+++ tasks.py
@@ -3,10 +3,10 @@ from invoke import task
 
 @task
 def lint(c):
-    c.run('poetry run pre-commit run -a')
-    c.run('poetry run yamllint --strict .')
-    c.run('npm run lint:css')
-    c.run('npm run lint:js')
-    c.run('npm run lint:md')
-    c.run('npm run lint:pug')
+    c.run("poetry run pre-commit run -a")
+    c.run("poetry run yamllint --strict .")
+    c.run("npm run lint:css")
+    c.run("npm run lint:js")
+    c.run("npm run lint:md")
+    c.run("npm run lint:pug")
     c.run("vale '--glob=*.md' docs")
